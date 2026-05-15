9054904d88a553e81a9efb562831f83760509097
clatabriv <claratab94@gmail.com>
2026-05-15 14:42:11 +0200

    Tabla de favoritos

--- frontendPet/src/app/components/favoritos/favoritos.ts
+++ frontendPet/src/app/components/favoritos/favoritos.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { ApiService } from '../../services/api';
 import { Animal } from '../../models/types';
 import { AuthService } from '../../services/auth';
@@ -9,7 +9,7 @@ import { NavbarComponent } from '../navbar/navbar';
 @Component({
   selector: 'app-favoritos',
   standalone: true,
-  imports: [RouterLink, NgFor, NavbarComponent],
+  imports: [RouterLink, NgFor, NgIf, NavbarComponent],
   templateUrl: './favoritos.html',
   styleUrl: './favoritos.css',
 })
@@ -44,44 +44,3 @@ export class FavoritosComponent implements OnInit {
     });
   }
 }
-// import { Component, OnInit } from '@angular/core';
-// import { RouterLink } from '@angular/router';
-// import { NgFor } from '@angular/common';
-// import { ApiService } from '../../services/api';
-// import { Animal } from '../../models/types';
-// import { AuthService } from '../../services/auth';
-// import { NavbarComponent } from '../navbar/navbar';
-
-// @Component({
-//   selector: 'app-favoritos',
-//   standalone: true,
-//   imports: [RouterLink, NgFor, NavbarComponent],
-//   templateUrl: './favoritos.html',
-//   styleUrl: './favoritos.css',
-// })
-// export class FavoritosComponent implements OnInit {
-//   favoritos: Animal[] = [];
-
-//   constructor(
-//     private apiService: ApiService,
-//     private authService: AuthService,
-//   ) {}
-
-//   ngOnInit(): void {
-//     this.recargar();
-//   }
-
-//   quitarFavorito(animalId: number): void {
-//     const user = this.authService.getCurrentUser();
-//     if (!user) {
-//       return;
-//     }
-//     this.apiService.eliminarFavorito(user.id, animalId).subscribe(() => this.recargar());
-//   }
-
-//   private recargar(): void {
-//     this.apiService.getMisFavoritos().subscribe({
-//       next: (animales) => (this.favoritos = animales),
-//     });
-//   }
-// }
--- frontendPet/src/app/components/refugios/refugios.ts
+++ frontendPet/src/app/components/refugios/refugios.ts
@@ -30,28 +30,3 @@ export class RefugiosComponent implements OnInit {
     this.router.navigate(['/refugios', refugioId]);
   }
 }
-// import { Component, OnInit } from '@angular/core';
-// import { RouterLink } from '@angular/router';
-// import { NgFor } from '@angular/common';
-// import { ApiService } from '../../services/api';
-// import { Usuario } from '../../models/types';
-// import { NavbarComponent } from '../navbar/navbar';
-
-// @Component({
-//   selector: 'app-refugios',
-//   standalone: true,
-//   imports: [RouterLink, NgFor, NavbarComponent],
-//   templateUrl: './refugios.html',
-//   styleUrl: './refugios.css',
-// })
-// export class RefugiosComponent implements OnInit {
-//   refugios: Usuario[] = [];
-
-//   constructor(private apiService: ApiService) {}
-
-//   ngOnInit(): void {
-//     this.apiService.getRefugios().subscribe({
-//       next: (refugios) => (this.refugios = refugios),
-//     });
-//   }
-// }
