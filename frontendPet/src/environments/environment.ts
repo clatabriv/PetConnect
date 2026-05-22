898efb4a2c877c8e345defc0e0ec0cc8e216131f
clatabriv <claratab94@gmail.com>
2026-05-22 13:10:28 +0200

    Apuntar a backend en producción

--- frontendPet/src/environments/environment.ts
+++ frontendPet/src/environments/environment.ts
@@ -1,4 +1,8 @@
+// export const environment = {
+//   production: true,
+//   apiUrl: 'http://localhost:8081/api',
+// };
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8081/api',
+  apiUrl: 'https://backendpet-pspy.onrender.com/api',
 };
