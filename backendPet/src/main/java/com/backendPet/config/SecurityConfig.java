9a3bfe477eeeacd26875234f6e9fbe1578e76f2e
clatabriv <claratab94@gmail.com>
2026-05-22 13:44:19 +0200

    Añadir dominio de Vercel a CORS

--- backendPet/src/main/java/com/backendPet/config/SecurityConfig.java
+++ backendPet/src/main/java/com/backendPet/config/SecurityConfig.java
@@ -44,12 +44,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-//      config.setAllowedOrigins(List.of("http://localhost:4200"));
-       config.setAllowedOrigins(List.of(
-           "http://localhost:4200",
-            "https://PetConnect.vercel.app" 
-        )); 
-config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedOrigins(List.of(
+            "http://localhost:4200",
+            "https://frontendpet.vercel.app"
+        ));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
         
