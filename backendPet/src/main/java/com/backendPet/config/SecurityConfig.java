9e401f7b79ff0b608db86d744c4ad146989f4c2a
clatabriv <claratab94@gmail.com>
2026-05-26 15:53:06 +0200

    Actualizar dominio en CORS

--- backendPet/src/main/java/com/backendPet/config/SecurityConfig.java
+++ backendPet/src/main/java/com/backendPet/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
             "http://localhost:4200",
-            "https://frontendpet.vercel.app"
+            "https://petconnect-clara.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
